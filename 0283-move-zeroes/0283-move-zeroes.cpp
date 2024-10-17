cce8ebb115e01438d1a8a40ea95803f6159ab6e4
Priyanshi Soni <sonipriyanshi.ps99@gmail.com>
2024-10-17 15:04:01 +0530

    Time: 24 ms (12.77%), Space: 21.9 MB (26.02%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+       int i = 0; // Pointer for the next position of non-zero element
+
+        for (int j = 0; j < nums.size(); j++) {
+            if (nums[j] != 0) {
+                // Swap only if nums[j] is non-zero
+                swap(nums[i], nums[j]);
+                i++; // Increment i for the next non-zero placement
+            }
+        }
+    }
+};

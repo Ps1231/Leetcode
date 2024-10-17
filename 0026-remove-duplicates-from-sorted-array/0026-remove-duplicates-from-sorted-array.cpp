ed192f3530e9fa0e67f92c2a8ca151f345f67dc9
Priyanshi Soni <sonipriyanshi.ps99@gmail.com>
2024-10-17 13:52:58 +0530

    Time: 0 ms (100.00%), Space: 21.1 MB (96.45%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
+    //Brute Force
+    // int removeDuplicates(vector<int>& nums) 
+    // {
+    //     set <int> hSet;
+    //     for (int i=0; i<nums.size();i++)
+    //     {
+    //         hSet.insert(nums[i]);
+    //     }
+    //     int setSize=hSet.size();
+    //     int j=0;
+    //     for (int x: hSet) 
+    //     {
+    //         nums[j++] = x;
+    //     }
+    //     return setSize;
+    // }
+    // Optimal Approch
     int removeDuplicates(vector<int>& nums) 
     {
-        set <int> hSet;
-        for (int i=0; i<nums.size();i++)
+    int i=0;
+        for (int x=1;x<nums.size();x++)
         {
-            hSet.insert(nums[i]);
-        }
-        int setSize=hSet.size();
-        int j=0;
-        for (int x: hSet) 
-        {
-            nums[j++] = x;
-        }
-        return setSize;
+           if(nums[x]!=nums[i]) 
+           {
+               i++;
+               nums[i]=nums[x];
+           }
+         
+        }return i+1;
     }
 };
     

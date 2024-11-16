bd3f66149ee5bdc8603a64a4df4eaa8f46eb6b58
Priyanshi Soni <sonipriyanshi.ps99@gmail.com>
2024-11-17 02:54:10 +0530

    Time: 0 ms (100.00%), Space: 19.6 MB (66.17%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n=nums.size();
-        map <int,int> hash;
-        for(int i=0;i<n;i++){
-            hash[nums[i]]++;
-        }
-        for (auto i : hash){
-            if ( i.second==1){ return i.first;}
-        }return -1;
-    }
+        int xor1=0;
+        for (auto x:nums)
+        {    xor1^=x; }
+        return xor1;   
+    } 
 };

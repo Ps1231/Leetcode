bd14a2eeae40c64e92ec57649b00d8056529dde1
Priyanshi Soni <sonipriyanshi.ps99@gmail.com>
2024-11-17 01:42:21 +0530

    Time: 1 ms (26.47%), Space: 21.1 MB (28.49%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,24 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-// better brute force 
+// better approach 
 class Solution 
 {
 public:
     int missingNumber(vector<int>& nums) 
-    {   int n=nums.size();
-     int flag;
-        for(int i=0;i<=n;i++)
+    {  int n=nums.size();
+     vector <int> arr(n+1);
+        
+        for(int i=0;i<n;i++)
         {
-            flag=0;
-            for(int j=0;j<=n-1;j++)
-            {
-                if( nums[j]==i){
-                    flag=1;
-                    break;
-                }
-            }
-             if (flag==0)return i;
-        } 
-     return -1;
+             arr[nums[i]]++;
+        }
+     for (int i = 0; i <= n; i++) 
+     {
+        if (arr[i] == 0) 
+        {
+            return i;
+        }
+    }return -1;
     }
 };

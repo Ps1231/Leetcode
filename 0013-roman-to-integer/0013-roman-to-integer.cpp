b0b3833563f36ddcee9e7cc7469cd0d53b330285
Priyanshi Soni <sonipriyanshi.ps99@gmail.com>
2025-02-09 22:46:55 +0530

    Time: 0 ms (100%), Space: 10.1 MB (86.89%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
-    int romanToInt(string s) {
-        unordered_map<char, int> m;
-        
-        m['I'] = 1;
-        m['V'] = 5;
-        m['X'] = 10;
-        m['L'] = 50;
-        m['C'] = 100;
-        m['D'] = 500;
-        m['M'] = 1000;
-        
-        int ans = 0;
-        
-        for(int i = 0; i < s.length(); i++){
-            if(m[s[i]] < m[s[i+1]]){
-                ans -= m[s[i]];
-            }
-            else{
-                ans += m[s[i]];
-            }
+int char2num(char a) {
+        switch (a) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
         }
-        return ans;
+    }
+    int romanToInt(string s) {
+        int result = 0;
+        int prev = 0; // Store the last processed value
+
+        // Reverse loop (right to left)
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int curr = char2num(s[i]);
+
+            if (curr < prev) {
+                result -= curr; // Subtract if smaller than the previous value
+            } else {
+                result += curr; // Otherwise, add
+            }
+
+            prev = curr; // Update previous value
+        }
+
+        return result;
+
     }
 };

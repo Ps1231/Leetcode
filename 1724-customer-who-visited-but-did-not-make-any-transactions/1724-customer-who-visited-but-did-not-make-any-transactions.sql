12070f32bb7466c25f04f5a810d3da2da5de454e
Priyanshi Soni <sonipriyanshi.ps99@gmail.com>
2025-03-07 23:20:57 +0530

    Time: 3024 ms (24.53%), Space: 0B (100%) - LeetHub

--- 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,5 +1,4 @@
 SELECT v.customer_id, COUNT(*) AS count_no_trans
 FROM Visits v
-LEFT JOIN Transactions t ON v.visit_id = t.visit_id
-WHERE t.visit_id IS NULL
+WHERE v.visit_id NOT IN (SELECT DISTINCT visit_id FROM Transactions)
 GROUP BY v.customer_id;

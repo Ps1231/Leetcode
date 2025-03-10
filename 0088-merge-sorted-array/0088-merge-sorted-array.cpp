3a465782b0347d6ad7dc710a96aba412a34ec3b1
Priyanshi Soni <sonipriyanshi.ps99@gmail.com>
2025-03-10 22:40:21 +0530

    Time: 0 ms (100%), Space: 12.4 MB (39.26%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,28 +1,44 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int total = m + n;
+        int gap = (total / 2) + (total % 2); // Initial gap
 
-    vector<int> arr3(m + n);
-    int left = 0, right = 0, index = 0;
-    while (left < m && right < n) {
-        if (nums1[left] <= nums2[right]) {
-            arr3[index] = nums1[left];
-            left++, index++;
+        auto nextGap = [](int gap) {
+            return (gap <= 1) ? 0 : (gap / 2) + (gap % 2);
+        };
+
+        while (gap > 0) {
+            int i = 0;
+
+            // Comparing elements in nums1
+            for (; i + gap < m; i++) {
+                if (nums1[i] > nums1[i + gap]) {
+                    swap(nums1[i], nums1[i + gap]);
+                }
+            }
+
+            // Comparing elements between nums1 and nums2
+            int j = gap > m ? gap - m : 0;
+            for (; i < m && j < n; i++, j++) {
+                if (nums1[i] > nums2[j]) {
+                    swap(nums1[i], nums2[j]);
+                }
+            }
+
+            // Comparing elements in nums2
+            for (j = 0; j + gap < n; j++) {
+                if (nums2[j] > nums2[j + gap]) {
+                    swap(nums2[j], nums2[j + gap]);
+                }
+            }
+
+            gap = nextGap(gap);
         }
-        else {
-            arr3[index] = nums2[right];
-            right++, index++;
+
+        // Copy nums2 to nums1
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
         }
     }
-    while (left < m) {
-        arr3[index++] = nums1[left++];
-    }
-    while (right < n) {
-        arr3[index++] = nums2[right++];
-    }
-    for (int i = 0; i < n + m; i++) {
-         nums1[i] = arr3[i];
-        
-    }
-}
 };

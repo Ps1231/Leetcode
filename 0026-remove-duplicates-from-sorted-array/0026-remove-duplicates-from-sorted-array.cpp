6b0901a15dd25e8c357548c583d896bb506da00b
Priyanshi Soni <sonipriyanshi.ps99@gmail.com>
2025-04-29 22:48:33 +0530

    Time: 0 ms (100%), Space: 22.5 MB (96.58%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -17,18 +17,43 @@ public:
     //     return setSize;
     // }
     // Optimal Approch
+    // int removeDuplicates(vector<int>& nums) 
+    // {
+    //     set <int> seth;
+    //     for(int i=0;i<nums.size();i++)
+    //     {
+    //         seth.insert(nums[i]);
+    //     }
+    //     int j=0;
+    //     for( int x:seth){
+    //         nums[j++]=x;
+    //     }
+    //     return seth.size();
+
+    // }
+
     int removeDuplicates(vector<int>& nums) 
     {
-    int i=0;
-        for (int x=1;x<nums.size();x++)
-        {
-           if(nums[x]!=nums[i]) 
-           {
-               i++;
-               nums[i]=nums[x];
-           }
+    // int i=0;
+    //     for (int x=1;x<nums.size();x++)
+    //     {
+    //        if(nums[x]!=nums[i]) 
+    //        {
+    //            i++;
+    //            nums[i]=nums[x];
+    //        }
          
-        }return i+1;
+    //     }return i+1;
+
+    int i=0;
+     for (int j=1; j<nums.size();j++){
+        if(nums[i]!=nums[j]){
+            i++;
+            nums[i]=nums[j];
+        }
+        
+
+     } return i+1;
     }
 };
     

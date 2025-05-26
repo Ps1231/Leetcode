cfae02b35040aa4433f12d459d00de2c74c57c20
Priyanshi Soni <sonipriyanshi.ps99@gmail.com>
2025-05-26 23:52:53 +0530

    Time: 410 ms (84.05%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,2 +1,9 @@
 # Write your MySQL query statement below
-select case when count(*)=0 then null else max(num) end as num from ( select num from MyNumbers group by num having count(num)=1 ) as test
+SELECT (
+    SELECT num 
+    FROM MyNumbers 
+    GROUP BY num 
+    HAVING COUNT(num) = 1 
+    ORDER BY num DESC 
+    LIMIT 1
+) AS num;
